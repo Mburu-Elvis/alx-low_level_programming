2f3d095e594c22a47394029201e2cece2a954a44
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2022-09-17 08:47:25 -0700

    ./8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**i
+ * main - main block
+ * Description: 8-print_base16.c
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	char c = 'a';
+	
+	while(i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	while(c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
