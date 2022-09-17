dfdc41672c09b843bfeb4a640a9e32d1374fc94d
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2022-09-17 03:20:19 -0700

    Betty Style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -11,14 +17,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0){
-		printf("%d is positive\n",n);
+	if (n>0) 
+	{
+		printf("%d is positive\n", n);
 	}
-	else if(n==0){
-		printf("%d is zero\n",n);
+	else if(n==0)
+	{
+		printf("%d is zero\n", n);
 	}
-	else{
-		printf("%d is negative\n",n);
+	else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
