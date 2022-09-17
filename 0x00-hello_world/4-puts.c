62123d15d8b0e9c94200a564f5ff48848c14dcfa
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2022-09-16 23:31:31 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Main function entr point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * description- starting point of the program
+ * Return: O as a success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); /* End of execution*/
+}
