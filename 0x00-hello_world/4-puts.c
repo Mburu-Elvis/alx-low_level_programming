2c654674fc315b50659f44130995eefd69252086
Mburu-Elvis <109366128+Mburu-Elvis@users.noreply.github.com>
2022-09-17 09:36:16 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
 /**
- * main - Main function entr point
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}#include <stdio.h>
-
-/**
  * main - this is the main function
  * description- starting point of the program
  * Return: O as a success
