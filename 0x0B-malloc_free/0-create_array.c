b65f48fc22ad86bd6c8a93a42c142212b2e861a9
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2022-10-04 12:56:11 -0700

    create  array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array of characters
+ * @size: the size of the memory to print
+ * @c: the size to be assgned to the address
+ */
+
+void *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+
+		if (!(i % 10) && i)
+		{
+			print("\n");
+		}
+		printf("0x%02x", c);
+		i++;
+	}
+	printf("\n");
+	
+}
+
+/**
+ * main - creates an array of chars and initialized
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char *buffer;
+
+	buffer = create_array(98,"H");
+	if (buffer == NULL)
+	{
+		printf("Failed to allocate memory\n");
+		return (1);
+	}
+	create_array(buffer, 98);
+	free(buffer);
+	return (0);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,4 @@
+#ifinder _HOLBERTON_H_
+#define _HOLBERTON_H
+
+char *create_array(unsigned int size, char c);_
