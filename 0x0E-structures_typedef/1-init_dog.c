93e85600b0aa696fa0b6c8d8b7628509d2f506a0
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2022-10-11 13:51:40 -0700

    Struct and Typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* init_dog - initializes a variable of the type dog
+* @d: The dog to be initialized
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The owner of the dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
