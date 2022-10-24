96895506e652c1fee27302321fce03b58c4a2038
Elvis Mburu <109366128+Mburu-Elvis@users.noreply.github.com>
2022-10-25 00:32:16 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,22 +6,14 @@
  * Return: no return.
  */
 
-
-
 void free_list(list_t *head)
 {
-	list_t *current_node;
+	list_t *current;
 
-	while ((current_node = head) != NULL)
-
+	while ((current = head) != NULL)
 	{
-
 		head = head->next;
-
-		free(current_node->str);
-
-		free(current_node);
-
+		free(current->str);
+		free(current);
 	}
 }
-Footer
