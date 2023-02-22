857b430d7cc477d95c81ba59a99593e41a8e912b
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-02-21 22:22:11 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - main block of the program
+ *
+ * Description: print _putchar foolwed by a new line
+ * Return: 0.
+ */
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+	int len = strlen(x);
+	
+	for (i = 0; i < len; i++)
+	{
+		putchar(x[i]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+#0x02-functions_nested_loops
+---
+
+1. **0-putchar.c**
+- use putchar function to print '_putchar' followed by a new line
