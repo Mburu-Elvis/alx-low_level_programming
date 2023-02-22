60a83e4a2411a2aef04b2338db89b29b241009ea
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-02-22 03:38:35 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @j: number being evaluated
+ *
+ * Return: last digit
+ */
+int print_last_digit(int j)
+{
+	int x = j % 10;
+
+	return (x);
+}
