e887835ed47d72898437ffdb72d46087059fd0ef
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-04 16:10:35 -0800

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,15 +21,16 @@ void puts_half(char *str)
 	{
 		half = (count / 2);
 	}
-	else if ((count % 2) == 1)
+	else if (count == 0)
+	{
+		_putchar('\0');
+		half = 0;
+	}
+	else
 	{
 		half = ((count - 1) / 2) + 1;
 	}
-	else if (count == 0)
-	{
-		half = 0;
-	}
-	while (str[half] != '\0')
+	while (str[half] != '\0'  && half != 0)
 	{
 		_putchar(str[half]);
 		half++;
