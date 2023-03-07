f1365f219db17b05b0ee4e6408b4d47b4c5a36ad
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-06 22:36:22 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string -a function that capitalizes a string
+ * @c: string being capitalized
+ *
+ * Description: a function that capitalizes a string
+ * Return: char
+ */
+char *cap_string(char *c)
+{
+	int low = 65, i = 0, diff;
+
+	while (c[i] != '\0')
+	{
+		if (i == 0 || c[i - 1] == 32)
+		{
+			if (c[i] >= 97 && c[i] <= 122)
+			{
+				diff = c[i] - 97;
+				c[i] = low + diff;
+			}
+			else
+			{
+				c[i] = c[i];
+			}
+		}
+		else
+		{
+			c[i] = c[i];
+		}
+		i++;
+	}
+	return (c);
+}
