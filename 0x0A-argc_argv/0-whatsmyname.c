641cfb6325f3ebf4c5174577e521a2e036a2e4ff
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-14 00:56:35 -0700

    0-whatsyourname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Description: prints the name of the program
  * Return: 0
  */
-int main (int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
