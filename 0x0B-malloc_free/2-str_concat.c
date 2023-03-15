a127a9a3fc8f73964b7900cb05d9cc850b823ea4
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-15 14:35:29 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,12 +14,18 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	int len, len1;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (len = 0; s1[len] != '\0'; len++)
 	{}
 	for (len1 = 0; s2[len1]; len1++)
 	{}
 	len1++;
 	s3 = malloc(sizeof(char) * (len + len1));
+	if (s3 == NULL)
+		return (NULL);
 	for (len = 0; s1[len] != '\0'; len++)
 		s3[len] = s1[len];
 	for (len1 = 0; s2[len1]; len1++)
