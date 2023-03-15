cd5c10916695dbf3df49f3cddf9e8e004a78c673
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-15 13:21:24 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a function to duplicate a string
+ * @str: the string to create a duplicate for
+ *
+ * Description: a function that creates and return a duplicate for str
+ * Return: duplicate
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int len;
+
+	if (str == NULL)
+		return (NULL);
+	dup = malloc(sizeof(str));
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		dup[len] = str[len];
+	}
+	dup[len] = '\0';
+	return (dup);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,6 @@
 
 1. **0-create_array**
 - a function that creates an array of chars and initializes it with a specific char
+
+2. **1-strdup.c**
+- a function that returns a pointer to a newly allocated space in memory, which contains a copy of a given string as a parameter
