892e2c0b6583c0f09931de2ea3e1a34631c97030
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-17 04:34:33 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len,i = 0;
-	for (len = 0; src[len] != '\0'; len++)
-	{}
+	int i;
 
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
-	dest[len] = '\0';
+	for (; i <  n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
