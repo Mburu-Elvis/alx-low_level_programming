aacaea4740b8e90ad15af59385c606ba08f292ac
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-22 11:54:03 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initializes a struct
+ * @d: the d dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ *
+ * Description: The initialization of the dog with all the attributes
+ * Return: None
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -4,3 +4,6 @@
 
 1. ***dog.h***
 - a struct dog with elements (name, age, owner)
+
+2. ***1-init_dog.c***
+- a function that initialize a variable of type struct dog
