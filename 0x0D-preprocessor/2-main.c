8d48de2027aa08b4dcba8d1df232afaa39ec614e
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-22 04:48:40 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - main function of the program
+ *
+ * Description: prints the file name
+ * Return: 0
+ */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
