8f3a67ce44804d6b64d3509a92f19edcdf00ac40
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-22 09:32:11 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct name - struct describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name pointer
+ *
+ * Description: struct desribing a dog's details
+ */
 struct name
 {
 	char *name;
