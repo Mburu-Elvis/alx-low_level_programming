edff33851c7c81fea6b0c5fdf994ae9698a45af0
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-23 15:11:29 -0700

    1-print_number.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,15 +21,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(ap, int);
-		if (i < (n - 1))
-		{
-			if (separator == NULL)
-				printf("%d ", num);
-			else
-				printf("%d%s ", num, separator);
-		}
-		else
-			printf("%d", num);
+		printf("%d", num);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
 	va_end(ap);
 	printf("\n");
