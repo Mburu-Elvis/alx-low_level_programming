0d7ec2f9a37a86739694ee63ddc55f1497287a26
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-03-31 01:31:17 -0700

    1-list_len.c edited

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,20 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
-		if (h->str != NULL && h->len != 0)
-		{
-			n += 2;
-			break;
-		}
-		else
-		{
-			n += 1;
-			break;
-		}
+		h = h->next;
+		nodes++;
 	}
-	return (n);
+	return (nodes);
 }
