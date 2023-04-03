4733e65c32600dce3f15964a5a27d1cf6d95fb12
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-03 08:18:11 -0700

    2-add_nodeint.c handled fail mem allocation

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,6 +13,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new = NULL;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
