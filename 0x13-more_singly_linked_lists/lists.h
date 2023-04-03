f722353eb018f90978f06a8a68dd31915af75232
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-03 05:44:46 -0700

    0-print_list.c lists.h README.md created

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s -singly linked list
+ * @n: integer
+ * @next: points to the the next node
+ *
+ * Description: Singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+#endif
