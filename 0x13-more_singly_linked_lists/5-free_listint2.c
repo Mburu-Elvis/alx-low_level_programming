5fe123e27a33144bce68a7b1a26a9bc1c8790fc3
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-04 12:54:10 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,6 +11,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *freeNode, *tempNode;
 
+	if (head == NULL)
+		return;
 	freeNode = *head;
 	while (freeNode != NULL)
 	{
@@ -19,4 +21,4 @@ void free_listint2(listint_t **head)
 		free(tempNode);
 	}
 	*head = NULL;
-}	
+}
