1d07ea94c454310eee6becbdb0c3b0a63d03c871
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-19 18:11:32 -0700

    1-array_iterator.c created

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "finction_pointers.h"
+
+/**
+ * array_iterator - execute a function given as a parameter
+ * @array: array to be used
+ * @size: the size of the array
+ * @action: pointer to function to use
+ *
+ * Return: None
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	int j = 0;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[j]);
+			j++;
+		}
+	}
+}
