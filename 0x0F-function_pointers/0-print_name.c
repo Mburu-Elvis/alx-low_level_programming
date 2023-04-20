20ff0f506c9dde627ff88f9b0815491887bb1631
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-19 17:21:41 -0700

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: pointer to the name string
+ * @f: a function pointer
+ *
+ * Return: None
+ */
+void print_name(char *name, void (*f) (char *))
+{
+	fun(*name);
+}
