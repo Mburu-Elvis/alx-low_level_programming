02f2ca87904bc523d1cc5549092acbfe5ac8e2a5
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-20 13:32:43 -0700

    debugging for check 6

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list print_no;
 	unsigned int i;
 
-	if (n == 0)
-		return;
 	va_start(print_no, n);
 	for (i = 0; i < n; i++)
 	{
@@ -22,7 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i < (n - 1))
 			printf("%d%s", va_arg(print_no, int), separator);
 		if (separator == NULL && i < (n - 1))
-			printf("%d", va_arg(print_no, int));
+		{
+			separator = "";
+			printf("%d%s", va_arg(print_no, int), separator);
+		}
 	}
 	va_end(print_no);
 }
