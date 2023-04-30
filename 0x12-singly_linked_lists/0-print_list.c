e1d173c2226cd1b768f52087919941011fc9c68e
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-30 04:38:27 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+* print_list - a function that prints all the elements of a list_t list
+* @h: the list of type list_l to print its elements
+*
+* Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
