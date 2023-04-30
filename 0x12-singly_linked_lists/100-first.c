16741b1fd451cfce8afaad1f6f1379bef420dcc7
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-04-30 07:51:44 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* before_main -  function to print sentences before executing main
+*
+* Return: None
+*/
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
