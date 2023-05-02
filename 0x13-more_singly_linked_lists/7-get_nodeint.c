95da8abeb77cc789278ab43d3994f874557bd547
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-05-02 15:03:10 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,16 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, len = 0;
 
 	if (head == NULL)
 		return (NULL);
-	while (i < index)
+	while (head != NULL && i < index)
 	{
 		head = head->next;
 		i++;
 	}
+	if (i < index)
+		return (NULL);
 	return (head);
 }
