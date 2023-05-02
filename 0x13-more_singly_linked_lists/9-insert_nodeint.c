d93a271efedb18e6e66ffaea7ae8a9047ecb2b27
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-05-02 16:10:08 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (temp == NULL || i < (idx - i))
 	{
-		free(newNode);
+		free(temp);
 		return (NULL);
 	}
 	newNode->next = temp->next;
