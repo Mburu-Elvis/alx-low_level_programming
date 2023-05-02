9449796af1edbcba4118e88ea63af9bd38c124d1
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-05-02 16:24:32 -0700

    9-insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,33 +10,39 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *newNode, *temp;
+    unsigned int i = 0;
+    listint_t *newNode, *temp;
 
-	if (head == NULL)
-		return (NULL);
-	newNode = malloc(sizeof(listint_t));
-	if (newNode == NULL)
-		return (NULL);
-	newNode->n = n;
-	if (idx == 0)
-	{
-		newNode->next = *head;
-		*head = newNode;
-		return (newNode);
-	}
-	temp = *head;
-	while (temp != NULL && i < (idx - 1))
-	{
-		temp = temp->next;
-		i++;
-	}
-	if (i < (idx - i))
-	{
-		free(newNode);
-		return (NULL);
-	}
-	newNode->next = temp->next;
-	temp->next = newNode;
-	return (newNode);
+    if (head == NULL)
+        return (NULL);
+
+    if (idx == 0)
+    {
+        newNode = malloc(sizeof(listint_t));
+        if (newNode == NULL)
+            return (NULL);
+        newNode->n = n;
+        newNode->next = *head;
+        *head = newNode;
+        return (newNode);
+    }
+
+    temp = *head;
+    while (temp != NULL && i < (idx - 1))
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    if (i != (idx - 1))
+        return (NULL);
+
+    newNode = malloc(sizeof(listint_t));
+    if (newNode == NULL)
+        return (NULL);
+    newNode->n = n;
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    return (newNode);
 }
