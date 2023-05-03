78c2561c9620d512ed65f54b1268fc2bf6bf8b72
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-05-02 17:20:31 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a linked list of integers (handles loops)
+ * @head: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *curr = head, *temp;
+
+	while (curr != NULL)
+	{
+		count++;
+		printf("[%p] %d\n", (void *)curr, curr->n);
+		temp = curr;
+		curr = curr->next;
+		if (temp <= curr)
+		{
+			printf("-> [%p] %d\n", (void *)curr, curr->n);
+			break;
+		}
+	}
+	return (count);
+}
