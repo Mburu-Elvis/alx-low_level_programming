c3209569dca3dd17bd5806ce844a533ca219183b
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-06-09 02:17:29 -0700

    renamed directory from 0x0A-argc-argv to 0x0A-argc_argv

---
+++ 0x08-recursion/100-is_palidrome.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_palindrome - function to check is a string is a palindrome
+ * @s: pointer to the string to evaluate
+ *
+ * Return: 1 if true else 0
+ */
+int is_palindrome(char *s)
+{
+
+}
+int len = 0;
+int str_rev(char *s)
+{
+	char *str;
+
+	if (_strlen_recursion(s) == 1)
+		return *str = *s;
+	else
+	{
+	(str) = *(s + _strlen_recursion
+
+
