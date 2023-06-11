316ab7550215630e1d7077524034ffaaf043606c
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-06-11 12:59:48 -0700

    5sqrt_recursion - finding the base case

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square-root of a number
+ * @n: number to evaluate square-root
+ *
+ * Return: the natural square-root of the number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+}
