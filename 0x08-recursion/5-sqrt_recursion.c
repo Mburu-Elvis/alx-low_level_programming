a78a7980584c6478d072fe6185421bdf20077de4
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-06-11 13:40:05 -0700

    5-square-root.c edge cases

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,4 +12,23 @@ int _sqrt_recursion(int n)
 		return (0);
 	if (n < 0)
 		return (-1);
+	return (helper(n));	
 }
+
+/**
+ * helper: helper function to square-rot function
+ * @n: number to be evaluated
+ *
+ * Return: square root
+ */
+int helper(int n)
+{
+	int i = 1;
+
+	if (i > n)
+		return (-1);
+	if ((i * i) == n)
+		return (i);
+	i++;
+	return helper(n);
+}	
