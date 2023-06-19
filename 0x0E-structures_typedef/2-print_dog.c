0b688e13bb28ff59e93b3cf35339741b174a1649
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-06-19 06:55:00 -0700

    2-print_dog.c debugging

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,6 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			printf("Name: %s\n", "(nil)");
-		if (!d->age)
-			printf("Age: %s\n", "(nil)");
 		if (d->owner == NULL)
 			printf("Owner: %s\n", "(nil)");
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
