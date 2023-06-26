f3c9f5db444c2b9fb13ad145d5d96cd1b74876c9
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-06-26 01:08:28 -0700

    3-print_all.c debugged

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,13 +10,13 @@ void print_all(const char * const format, ...)
 {
 	int i = 0;
 	va_list ap;
-	char *str, spec, args[] = {'c', 'i', 'f', 's'};
+	char *str, c;
 
 	va_start(ap, format);
-	while (format[i])
+	while (format[i] && format != NULL)
 	{
-		spec = args[i];
-		switch (spec)
+		c = format[i];
+		switch (c)
 		{
 			case 'c':
 				printf("%c", va_arg(ap, int));
@@ -35,6 +35,8 @@ void print_all(const char * const format, ...)
 				break;
 		}
 		i++;
+		if ((c == 'c' || c == 'i' || c == 'f' || c == 's') && format[i])
+			printf(", ");
 	}
 	va_end(ap);
 	printf("\n");
