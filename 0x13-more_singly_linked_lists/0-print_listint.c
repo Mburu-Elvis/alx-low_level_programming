6bc66a9a02f775f1e01e5456897cb6b0f0ac42be
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-03 12:38:24 -0700

    betty 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * prist_listint - function that prints all the elements of a listint_t list
+ * print_listint - function that prints all the elements of a listint_t list
  * @h: list whose elements are to be printed
  *
  * Return: The number of nodes
@@ -10,13 +10,16 @@ size_t print_listint(const listint_t *h)
 {
 	int elem = 0;
 
-	while (h->next != NULL)
+	if (h != NULL)
 	{
+		while (h->next != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			elem++;
+		}
 		printf("%d\n", h->n);
-		h = h->next;
 		elem++;
 	}
-	printf("%d\n", h->n);
-	elem++;
 	return (elem);
 }
