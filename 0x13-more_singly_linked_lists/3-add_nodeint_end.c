54788304e8992a7853872ae0f4e7b41b9f23a78d
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-03 14:04:32 -0700

    betty 3-add_nodeint.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,7 +25,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	temp = *head;
 	while (temp->next != NULL)
-		temp = temp->next; 
+		temp = temp->next;
 	temp->next = new_node;
 	return (new_node);
 }
