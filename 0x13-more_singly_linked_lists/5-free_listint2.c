a9c3fbfafd67c1708bc8203e6846c496dd19c5a1
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-04 00:32:05 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: head pointer to the list to free
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head != NULL)
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			*head = temp->next;
+			free(temp);
+			temp = *head;
+		}
+		free(temp);
+	}
+}
