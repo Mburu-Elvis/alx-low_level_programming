9c7f1ab17c08f08960b589bc699d81fb2c0a338c
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-05 12:50:44 -0700

    debugging insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				return (new_node);
 			}
 		}
-		if (temp->next == NULL && elem > idx)
+		if (idx > elem)
 			return (NULL);
 	}
 	return (NULL);
