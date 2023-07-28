be88b9f95656421e45f5281526c6548bb6dbd9cb
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-27 23:04:00 -0700

    task 0 debugging

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,47 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * read_textfile - reads a text file and prints to the STDOUT
+ * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
- * @letters: maximum number of letters to be read and printed
+ * @letters: number of letters to read and print
  *
- * Return: actual number of letters it could read and print
- * if filename is NULL return 0
- * if write fails return 0
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, length, i, result;
+	int fd, bytes_read, written;
 	char *buffer;
 
 	if (filename == NULL)
+
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
+
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (0);
-	read(fd, buffer, letters);
-	buffer[letters] = '\0';
-	for (i = 0; buffer[i] != '\0'; i++)
-		length += 1;
-	result = close(fd);
-	if (result != 0)
-		exit(-1);
-	result = write(STDOUT_FILENO, buffer, length);
-	if (result != length)
-		return (0);
-	free(buffer);
 
-	return (length);
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[bytes_read] = '\0';
+	close(fd);
+
+	written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (written < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (written);
+
 }
