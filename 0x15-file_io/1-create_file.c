d088b1645216cf8dde694df38b1c04d4bac69787
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-27 23:11:45 -0700

    1-create_file.c debugging

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
 #include "main.h"
 
 /**
- * create_file - function that creates a file
- * @filename: the name of the file to create
- * @text_content: string to write to the file
- * Description: function that creates a file
- * Return: 1 on success, else -1
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fd;
+	ssize_t written;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = '\0';
-	fd = fopen(filename, "w+");
-	if (fd == NULL)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-	fprintf(fd, "%s", text_content);
-	fclose(fd);
-	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
-		return (-1);
+	if (text_content != NULL)
+	{
+		written = write(fd, text_content, strlen(text_content));
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	 close(fd);
 	return (1);
 }
