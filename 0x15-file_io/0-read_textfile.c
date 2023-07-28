409b4a196b101845e34bcb067bdeed826c386e96
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-07-27 22:59:54 -0700

    task 0 debugging

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,38 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - function that read and prints the content of a file
- * @filename: the name of the file to read and print letters
- * @letters: the number of letters to be read and printed
+ * read_textfile - reads a text file and prints to the STDOUT
+ * @filename: name of the file to be read
+ * @letters: maximum number of letters to be read and printed
  *
- * Return: actual numbers printed, else 0
+ * Return: actual number of letters it could read and print
+ * if filename is NULL return 0
+ * if write fails return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fd;
-	char *buf;
-	ssize_t act_read, act_print;
+	int fd, length, i, result;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	fd = fopen(filename, "r");
-	if (fd == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * (letters + 1));
-	if (buf == NULL)
-	{
-		fclose(fd);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
-	}
-	act_read = fread(buf, sizeof(char), letters, fd);
-	if (act_read <= 0)
-	{
-		fclose(fd);
-		free(buf);
+	read(fd, buffer, letters);
+	buffer[letters] = '\0';
+	for (i = 0; buffer[i] != '\0'; i++)
+		length += 1;
+	result = close(fd);
+	if (result != 0)
+		exit(-1);
+	result = write(STDOUT_FILENO, buffer, length);
+	if (result != length)
 		return (0);
-	}
-	act_print = fwrite(buf, sizeof(char), act_read, stdout);
-	if (act_read != act_print)
-	{
-		fclose(fd);
-		free(buf);
-		return (0);
-	}
-	fclose(fd);
-	free(buf);
-	return (act_read);
+	free(buffer);
+
+	return (length);
 }
