5d335dd586f2a68527500d13922bbeb5164e53b4
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-17 16:53:03 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,9 +14,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	while (n <= index)
+	while (temp != NULL)
 	{
-		if (n == index && temp != NULL)
+		if (n == index)
 			return (temp);
 		temp = temp->next;
 		n++;
