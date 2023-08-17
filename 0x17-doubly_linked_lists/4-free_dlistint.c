1d76a743a96daa9f7a24efd4f4ea7d5f062d42f8
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-17 11:56:25 -0700

    4-frree_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - function that frees a doubly linked list
+ * @head: the first node of the list
+ *
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
