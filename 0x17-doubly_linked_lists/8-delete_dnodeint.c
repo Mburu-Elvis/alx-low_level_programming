90fdfa9d9a1ad9e6dc291e9ce965c51f04fab5f4
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-17 16:35:28 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,10 +14,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
 	while (n <= index)
 	{
-		if (temp->next == NULL && n != index)
-			return (-1);
 		if (n == index)
 		{
 			prev = temp->prev;
